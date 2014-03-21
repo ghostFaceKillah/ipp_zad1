af159861761122cafc67f574d9bae0c53a9b499f
ghostFaceKillah <garmulewicz@gmail.com>
2014-03-21 23:41:34 +0100

    cleanup and refactor halfway done. Some nice touches before grading

--- drzewa.pas
+++ drzewa.pas
@@ -5,26 +5,9 @@ unit drzewa;
 
 interface
 
-const
-  MAX_TREE_NUM = 1000000;
-
-type
-  tree = ^node;
-  node = record
-    x : LongInt;
-    y : LongInt;
-    l_sum : LongInt;
-    ref_count : integer;
-    left : tree;
-    right : tree;
-  end;
-
-  the_trees = array[0..MAX_TREE_NUM] of tree;
-  // for holding trees at diffrent moments of time 
-
-  procedure inicjuj;
-  // run at the beginning. Nils array of trees, sets up node counter and 
-  // used array space counter 
+  procedure inicjuj();
+  // run at the beginning. nils array of trees, sets up node node_counter and 
+  // used array space node_counter 
 
   function przypisanie(x, y : LongInt) : LongInt;
   // used for handling valid assignment. Returns current number of nodes
@@ -35,12 +18,8 @@ type
   function czysc(nr_funkcji : LongInt) : LongInt;
   // used for handling valid 'czysc' calls
 
-  function treeExists(const i : LongInt) : boolean;
+  function treeExists(const i : LongInt) : Boolean;
   // used for checking if given tree exists
-
-  procedure writeTree(const t : tree; i : integer);
-  // used for debugging this module. Writes given tree, which starts at depth i.
-  // For writing tree from the root use writeTree(t, 0); 
 
   procedure writeTrees();
   // used for debugging. Writes all trees in memory from the begging to now.
@@ -50,234 +29,230 @@ type
 
 implementation
 
+  const
+    MAX_TREE_NUM = 1000000;
+  
   type
-    linkage = (left, right, both);
-    // this is datatype used for passing which subtrees should be linked unchanged
+    Tree = ^Node;
+    Node = record
+      x : LongInt;
+      y : LongInt;
+      l_sum : LongInt;
+      // sum of all ys in the left subtree
+      ref_count : Integer;
+      left : Tree;
+      right : Tree;
+    end;
+
+    TheTrees = Array[0..MAX_TREE_NUM] of Tree;
+    // for holding trees at diffrent moments of time 
 
   var
-    trees : the_trees;
+    trees : TheTrees;
     // array for holding all the trees
-    counter : LongInt;
+    node_counter : LongInt;
     // how many distinct nodes we have allocated
-    vec_pos : integer;
+    functions_counter : Integer;
     // where in trees array 'time' we are (how many trees there are)
   
   procedure inicjuj();
     var
       i : LongInt;
     begin
-      counter := 0;
-      vec_pos := 0;
+      node_counter := 0;
+      functions_counter := 0;
       for i := 0 to MAX_TREE_NUM do
-        trees[i] := NIL;
+        trees[i] := nil;
     end;
 
-  function wrap(const x, y : LongInt) : tree;
+  function wrap(const x, y, l_sum : LongInt) : Tree;
     var
-      resu : tree;
+      resu : Tree;
     begin
       new(resu);
-      inc(counter);
+      inc(node_counter);
       resu^.x := x;
       resu^.y := y;
-      resu^.l_sum := 0;
+      resu^.l_sum := l_sum;
       resu^.ref_count := 1;
-      resu^.left := Nil;
-      resu^.right := Nil;
+      resu^.left := nil;
+      resu^.right := nil;
       wrap := resu;
     end;
 
-  function copyTree(const t : tree) : tree;
-    var
-      resu : tree;
+  function copyTree(const t : Tree) : Tree;
     begin
-      new(resu);
-      inc(counter);
-      resu^.x := t^.x;
-      resu^.y := t^.y;
-      resu^.l_sum := t^.l_sum;
-      resu^.ref_count := 1;
-      resu^.right := nil;
-      resu^.left := nil;
-      copyTree := resu;
+      copyTree := wrap(t^.x, t^.y, t^.l_sum);
     end;
 
-  procedure link(var previous : tree; var current : tree; const dir : linkage);
-    // used for automatic linking of unchanged subtrees. Please note usage of 
-    // custom enum type linkage= (left, right, both)
+  procedure link(var previous : Tree; var current : Tree; 
+                 const which_direction : String);
+    // used for automatic linking of unchanged subtrees. 
     begin
-      if (previous = NIL) or (current = NIL) then
+      if (previous = nil) or (current = nil) then
         writeln('ERROR LINKING');
-      if dir = both then begin
-        current^.right := previous^.right;
+      if (which_direction = 'left') or (which_direction = 'both') then begin
         current^.left := previous^.left;
-        if previous^.left <> Nil then
-	  inc(previous^.left^.ref_count);
-        if previous^.right <> Nil then
-	  inc(previous^.right^.ref_count);
-      end else if dir = left then begin
-        current^.left := previous^.left;
-        if previous^.left <> Nil then
-	  inc(previous^.left^.ref_count);
-      end else begin
+        if previous^.left <> nil then
+          inc(previous^.left^.ref_count);
+      end;
+      if (which_direction = 'right') or (which_direction = 'both') then begin
         current^.right := previous^.right;
-        if previous^.right <> Nil then
-	  inc(previous^.right^.ref_count);
+        if (previous^.right <> nil) then
+          inc(previous^.right^.ref_count);
       end;
     end;
 
-  function findSmallestNode(const t : tree ) : tree;
+  function findSmallestNode(t : Tree) : Tree;
     // finds node with smallest x in the tree
-    var
-      temp : tree;
     begin 
-      temp := t;
-      while temp^.left <> Nil do
-        temp := temp^.left;
-	findSmallestNode := temp;
+      assert(t <> nil);
+      while t^.left <> nil do
+        t := t^.left;
+      findSmallestNode := t;
     end;
 
   function przypisanie(x, y : LongInt) : LongInt;
 
-    function treeChanges(const t : tree; const x,y : LongInt) : integer;
+    function howMuchYChanges(const t : tree; const x, y : LongInt) : Integer;
       // subroutine used for calculating if and how much the tree changes,
       // measured in abs of y change.
       begin
-	if t = nil then
-	  treeChanges := y
-	else if (t^.x = x) and (t^.y = y) then
-	  treeChanges := 0
-	else if (t^.x = x) and (t^.y <> y) then
-	  treeChanges := y - t^.y
-	else if (t^.x < x) then
-	  treeChanges := treeChanges(t^.right,x,y)
-	else
-	  treeChanges := treeChanges(t^.left,x,y);
+        if (t = nil) then
+          howMuchYChanges := y
+        else if (t^.x = x) then
+          howMuchYChanges := y - t^.y
+        else if (t^.x < x) then
+          howMuchYChanges := howMuchYChanges(t^.right, x, y)
+        else
+          howMuchYChanges := howMuchYChanges(t^.left, x, y);
       end;
 
-    procedure processing(var previous, current : tree; x,y : LongInt);
+    procedure processing(var previous, current : Tree; x, y : LongInt);
       // first it calculates how much and where the previous tree should change
       // Later it applies the change. It also links the unchaged subtrees on
       // the go, using link() procedure.
+      // REWRITE DELETING 
+      // REFACTOR INTO THREE SMALL FUNCS
       var
-	temp : tree;
-	change : integer;
+        temp : Tree;
+        change_of_y : Integer;
       begin
-	change := treeChanges(previous, x, y);
-        // calculate if tree changes
-	if change <> 0 then begin
+        change_of_y := howMuchYChanges(previous, x, y);
+        // calculate if tree change_of_ys
+        if (change_of_y <> 0) then begin
           current := copyTree(previous); 
-          // current (at the moment t)  node is same as in t-1, but with small 
-          // changes that are as follows:
+          // current (at the moment t)  node is same as in t - 1, but with small 
+          // modificationsthat are as follows:
           if previous^.x = x then begin
             // current node should get a new y
-	    if y <> 0 then begin 
+            if y <> 0 then begin 
               // just overwrite the old y
               current^.y := y;
-              link(previous, current, both);
-	    end else begin
+              link(previous, current, 'both');
+            end else begin
               // kill the current node, using algorithm described on moodle
-	      if previous^.right = Nil then begin
-                // if right son is empty, we just dispose and link to the left
-	        dispose(current);
-		dec(counter);
-	        current := previous^.left;
-		if previous^.left <> Nil then
-		  inc(previous^.left^.ref_count);
-	      end else begin
+              if (previous^.right = nil) then begin
+                // if right child is empty, we just dispose and link to the left
+                dispose(current);
+                dec(node_counter);
+                current := previous^.left;
+                if (previous^.left <> nil) then
+                  inc(previous^.left^.ref_count);
+              end else begin
                 // we fill the content of current node with content of smallest
                 // x element from the right subtree ...
-	        temp := findSmallestNode(previous^.right);
-	        current^.x := temp^.x;
-	        current^.y := temp^.y;
+                temp := findSmallestNode(previous^.right);
+                current^.x := temp^.x;
+                current^.y := temp^.y;
                 // ... and dispose of the node,  which contents
                 //  were taken, by setting its y to 0 and processing it
-	        x := current^.x;
-		y := 0;
-	        processing(previous^.right, current^.right, x, y);
-                link(previous, current, left);
-	      end
-	    end
-          end else if previous^.x < x then
+                x := current^.x;
+                y := 0;
+                processing(previous^.right, current^.right, x, y);
+                link(previous, current, 'left');
+              end
+            end
+          end else if (previous^.x < x) then
             // go right looking for right place for the node
-            if previous^.right = Nil then begin
+            if (previous^.right = nil) then begin
               // free space in the tree to insert the node
-              current^.right := wrap(x,y);
-              link(previous, current, left);
+              current^.right := wrap(x, y, 0);
+              link(previous, current, 'left');
             end else begin
               // no space here, keep looking
-	      processing(previous^.right, current^.right, x, y);
-              link(previous, current, left);
-	    end
+              processing(previous^.right, current^.right, x, y);
+              link(previous, current, 'left');
+            end
           else begin
-	    current^.l_sum := current^.l_sum + change;
-            // go left, but remember to change l_sum
-            if previous^.left = Nil then begin
+            current^.l_sum := current^.l_sum + change_of_y;
+            // go left, but remember to modify l_sum
+            if (previous^.left = nil) then begin
               // some space here, insert
-              current^.left := wrap(x,y);
-              link(previous, current, right);
+              current^.left := wrap(x, y, 0);
+              link(previous, current, 'right');
             end else begin
               // no space here
-	      processing(previous^.left, current^.left, x, y);
-              link(previous, current, right);
-	    end;
-	  end;
+              processing(previous^.left, current^.left, x, y);
+              link(previous, current, 'right');
+            end;
+          end;
         end else begin
           // the new tree is same as old one, so we just link to it
-	  current := previous;
-	  inc(previous^.ref_count);
-	end;
+          current := previous;
+          inc(previous^.ref_count);
+        end;
       end;
 
     begin
       // now we just need to call processing() with good args
-      inc(vec_pos);
-      if (trees[vec_pos-1] = nil) then begin
-        // no t-1 tree to compare to, so we make a new one
-        trees[vec_pos] := wrap(x,y);
+      inc(functions_counter);
+      if (trees[functions_counter - 1] = nil) then begin
+        // no t - 1 tree to compare to, so we make a new one
+        trees[functions_counter] := wrap(x, y, 0);
       end else
-	processing(trees[vec_pos-1], trees[vec_pos], x, y);
-      przypisanie := counter;
+        processing(trees[functions_counter - 1], trees[functions_counter], x, y);
+      przypisanie := node_counter;
     end;
 
-  procedure writeTree(const t : tree; i : integer);
+  procedure writeTree(const t : Tree; i : Integer);
     var
-      j : integer;
+      j : Integer;
     begin
       for j := 0 to i do
         write('   ');
-      if t <> Nil then begin
-	writeln('f(', t^.x, ')=', t^.y,' l_sum=', t^.l_sum);
+      if (t <> nil) then begin
+        writeln('f(', t^.x, ')=', t^.y,' l_sum=', t^.l_sum);
         writeTree(t^.left, i+1);
         writeTree(t^.right, i+1);
       end else
-	writeln('nil');
+        writeln('nil');
     end;
 
   procedure writeTrees();
     var 
-      j : integer;
+      j : Integer;
     begin
-      for j := 0 to vec_pos do begin
+      for j := 0 to functions_counter do begin
         writeTree(trees[j], 0);
-	writeln();
+        writeln();
       end;
-      writeln('Current number of nodes is: ', counter);
+      writeln('Current number of nodes is: ', node_counter);
     end;
 
-  procedure cleanse(var t : tree);
-    // worker function for wrapper czysc. Decreases the reference counter
+  procedure cleanse(var t : Tree);
+    // worker function for wrapper czysc. Decreases the reference node_counter
     // and when ref count is zero, node should be disposed.
     begin
-      if t <> Nil then begin
+      if (t <> nil) then begin
         dec(t^.ref_count); 
         if (t^.ref_count = 0) then begin
-	  cleanse(t^.left);
-	  cleanse(t^.right);
-	  dispose(t);
-	  dec(counter);
-	  t := Nil;
-	end;
+          cleanse(t^.left);
+          cleanse(t^.right);
+          dispose(t);
+          dec(node_counter);
+          t := nil;
+        end;
       end;
     end;
 
@@ -286,7 +261,7 @@ implementation
     begin
       cleanse(trees[nr_funkcji]);
       trees[nr_funkcji] := nil;
-      czysc := counter;
+      czysc := node_counter;
     end;
 
   function findLastSmallerOrEq(x : LongInt; t : tree) : LongInt;
@@ -294,41 +269,34 @@ implementation
       resu : LongInt;
     begin
       resu := 0;
-      while t <> Nil do
-        if t^.x = x then begin
-	  t := Nil;
-	  resu := x;
-        end else if t^.x > x then
-	  t := t^.left
-	else begin
-	  resu := t^.x;
-	  t := t^.right;
-	end;
+      while t <> nil do
+        if (t^.x = x) then begin
+          t := nil;
+          resu := x;
+        end else if (t^.x > x) then
+          t := t^.left
+        else begin
+          resu := t^.x;
+          t := t^.right;
+        end;
       findLastSmallerOrEq := resu;
     end;
   
-  function partialSum(x : LongInt; t : tree) : LongInt;
+  function partialSum(x : LongInt; t : Tree) : LongInt;
+    // returns sum of y for x from 1 to  x, using sums of left subtrees
     // please note that x is _always_ in the tree, because its the maximal
     // smaller or equal than 'real' x element not the 'real' x.
-    // finding partial sum from arg=0 to arg=0 is easy when we keep correct
-    // l_sums. We search for x in the tree, and every time we go right,
-    // we need to add l_sum and value of current node to the result
-    // (because it is BST tree we are sure all of the nodes in left subtree
-    // are smaller than x. When we go left, we are sure current elem should not
-    // be added to the result, and when we find the wanted element addition
-    // if its value and sum of its left subtree is obvious, as nodes there
-    // are smaller
     var
       sum : LongInt;
     begin
       sum := 0;
-      if x <> 0 then begin
+      if (x <> 0) then begin
         while t^.x <> x do 
-         if x > t^.x then begin
-           sum := sum + t^.y + t^.l_sum;
-           t := t^.right;
-         end else
-           t := t^.left;
+          if (x > t^.x) then begin
+            sum := sum + t^.y + t^.l_sum;
+            t := t^.right;
+          end else
+            t := t^.left;
         sum := sum + t^.y + t^.l_sum;
       end;
       partialSum := sum;
@@ -338,7 +306,7 @@ implementation
     var
       left_bound, right_bound : LongInt;
     begin
-      if trees[nr_funkcji] = Nil then
+      if trees[nr_funkcji] = nil then
         suma := 0
       else begin
         left_bound := findLastSmallerOrEq(lewy_argument - 1, trees[nr_funkcji]);
@@ -348,20 +316,18 @@ implementation
       end;
     end;
 
-  function treeExists(const i : LongInt) : boolean;
+  function treeExists(const i : LongInt) : Boolean;
     begin
-      if i > vec_pos then
-        treeExists := false
-      else
-        treeExists := true
+      treeExists := (i <= functions_counter);
     end;
 
   procedure finalCleanup();
     var
       i : LongInt;
     begin
-      for i := 0 to vec_pos do
+      for i := 0 to functions_counter do
         cleanse(trees[i]);
+      assert(node_counter = 0);
     end;
 
 end.
--- zad1.pas
+++ zad1.pas
@@ -5,130 +5,135 @@
 program ipp_zad1;
 
 uses drzewa;
-  // a module for handling tree operations and tree memory of the func
 
 const
-  CHAR_OFFSET = 48; // for casting char to int
+  CHAR_OFFSET = 48; // for casting Char to Int
   MAX_INPUT_LINES = 1000000; // given in the task statement
+  // REWRITE - MAX VALID STATEMENTS 
+  MAX_X_VAL = 1000000000;
+  MAX_Y_VAL = 1000;
 
 procedure ignore();
   begin
     writeln('zignorowano');
   end;
   
-function isDigit(const c : char) : boolean;
-  // checks if ASCII char is digit
+function isDigit(const c : Char) : Boolean;
+  // checks if ASCII Char is digit
   begin
-    if (ord(c)<48) or (ord(c)>57) then isDigit:=false else isDigit:=true;
+    if (c < '0') or (c > '9') then
+      isDigit:=false
+    else
+      isDigit:=true;
   end;
   
-function char_to_int(c : char) : LongInt;
-  // casts char to int
+function charToInt(c : Char) : LongInt;
+  // casts Char to Int
   begin 
-    char_to_int := ord(c) - CHAR_OFFSET;
+    charToInt := ord(c) - ord('0');
   end;
 
 function parseLongInt(const from, too : Integer; const inp : String) : LongInt;
-  // parses non-negativeLongInt from a part of a string. If it cannot parse a 
-  // valid string or the string has leading zeros, then it returns -1 
+  // parses non-negativeLongInt from a part of a String. If it cannot parse a 
+  // valid String or the String has leading zeros, then it returns - 1 
   var
-    i : integer;
+    i : Integer;
     resu : LongInt;
-    keepGoing : boolean;
+    keep_going : Boolean;
   begin
     resu := 0;
     i := from;
-    keepGoing := true;
-    while keepGoing and (i <= too) do begin
+    keep_going := true;
+    while keep_going and (i <= too) do begin
       if isDigit(inp[i]) then
-        if (i-from > 0) and (resu = 0) then begin
+        if (i - from > 0) and (resu = 0) then begin
           resu := -1;
-          keepGoing := false;
+          keep_going := false;
         end else begin
-          resu := resu*10 + char_to_int(inp[i]);
+          resu := resu*10 + charToInt(inp[i]);
           inc(i);
         end
       else begin
         resu := -1;
-        keepGoing := false;
+        keep_going := false;
       end;
     end;
     parseLongInt := resu;
   end;
   
-procedure processPossibleFuncValue(const current_input : string);
+procedure processPossibleFuncValue(const input_line : String);
   // checks for correct func val assignment call
   var
-    eqPos : integer;
+    eq_pos : Integer;
     x, y : LongInt;
   begin
-    eqPos := 3;
-    while (eqPos <= length(current_input)) and (current_input[eqPos] <> '=') do
-      inc(eqPos);
-    if (eqPos >= length(current_input)) or
-       (current_input[eqPos-1] <> ':') or
-       (current_input[eqPos-2] <> ')') then
+    eq_pos := 3;
+    while (eq_pos <= length(input_line)) and (input_line[eq_pos] <> '=') do
+      inc(eq_pos);
+    if (eq_pos >= length(input_line)) or
+       (input_line[eq_pos - 1] <> ':') or
+       (input_line[eq_pos - 2] <> ')') then
       ignore()
     else begin
-      x := parseLongInt(3, eqPos-3, current_input);
-      y := parseLongInt(eqPos+1, length(current_input), current_input);
-      if (x = -1) or (y = -1) or (y > 1000) or (x > 1000000000) then
+      x := parseLongInt(3, eq_pos - 3, input_line);
+      y := parseLongInt(eq_pos+1, length(input_line), input_line);
+      if (x = -1) or (y = -1) or (y > MAX_Y_VAL) or (x > MAX_X_VAL) then
         ignore()
       else begin
-        writeln('wezlow: ', przypisanie(x,y));
+        writeln('wezlow: ', przypisanie(x, y));
       end;
     end;
   end;
 
-procedure processPossibleSum(const current_input : string);
+procedure processPossibleSum(const input_line : String);
   // checks for a correct pattern suma(t,a..b) call
   var
-    commaPos : integer;
-    pointPos : integer;
+    comma_pos : Integer;
+    point_pos : Integer;
     a, b, t : LongInt;
     s : LongInt;
   begin
-    commaPos := 6;
-    while (commaPos <= length(current_input)) and
-    (current_input[commaPos] <> ',') do
-      inc(commaPos);
-    pointPos := commaPos+1;
-    while (pointPos <= length(current_input)) and
-    (current_input[pointPos] <> '.') do
-      inc(pointPos);
-    if (commaPos = 6) or // no t present
-       (commaPos > length(current_input)) or
-       (pointPos > length(current_input)) or
-       (pointPos = commaPos + 1) or         // no a present
-       (current_input[pointPos+1] <> '.') or
-       (current_input[length(current_input)] <> ')') or
-       (length(current_input) <= pointPos + 2) then // no b present
+    comma_pos := 6;
+    while (comma_pos <= length(input_line)) and
+          (input_line[comma_pos] <> ',') do
+      inc(comma_pos);
+    point_pos := comma_pos+1;
+    while (point_pos <= length(input_line)) and
+          (input_line[point_pos] <> '.') do
+      inc(point_pos);
+    if (comma_pos = 6) or // no t present
+       (comma_pos > length(input_line)) or
+       (point_pos > length(input_line)) or
+       (point_pos = comma_pos + 1) or         // no a present
+       (input_line[point_pos + 1] <> '.') or
+       (input_line[length(input_line)] <> ')') or
+       (length(input_line) <= point_pos + 2) then // no b present
     ignore()
     else begin
-      t := parseLongInt(6, commaPos-1, current_input);
-      a := parseLongInt(commaPos+1, pointPos-1 , current_input);
-      b := parseLongInt(pointPos+2, length(current_input)-1, current_input);
-      if (t=-1) or (a=-1) or
-         (b=-1) or not(treeExists(t)) or
+      t := parseLongInt(6, comma_pos - 1, input_line);
+      a := parseLongInt(comma_pos + 1, point_pos - 1 , input_line);
+      b := parseLongInt(point_pos + 2, length(input_line) - 1, input_line);
+      if (t = -1) or (a = -1) or
+         (b = -1) or not(treeExists(t)) or
          (a > 1000000000) or (b > 1000000000) then
         ignore()
       else begin
-        s := suma(t,a,b);
+        s := suma(t, a, b);
         if s <> -1 then
           writeln('suma(', t, ',', a, '..', b, ')=', s);
       end
     end;
   end;
   
-procedure processPossibleCleanse(const current_input : string);
+procedure processPossibleCleanse(const input_line : String);
   // checks for a valid 'czysc(t)' pattern call
   var
     t : LongInt;
   begin
-    if length(current_input) <= 7 then
+    if length(input_line) <= 7 then
       ignore()
     else begin
-      t := parseLongInt(7, length(current_input)-1, current_input);
+      t := parseLongInt(7, length(input_line) - 1, input_line);
       if (t= -1) or not(treeExists(t)) then
         ignore()
       else begin
@@ -138,31 +143,29 @@ procedure processPossibleCleanse(const current_input : string);
   end;
 
 var
-  current_input : string;
-  input_count : LongInt;
+  input_line : String;
+  valid_input_count : LongInt;
 
 
 begin
   inicjuj();
-  input_count := 0;
-  current_input := '0';
+  valid_input_count := 0;
+  input_line := '0';
   while not(eof) do begin
-    if input_count < MAX_INPUT_LINES then begin
-      inc(input_count);
-      readln(current_input);
-      if copy(current_input, 1,2) = 'f(' then
-        processPossibleFuncValue(current_input)
-      else if copy(current_input, 1, 5) = 'suma(' then
-        processPossibleSum(current_input)
-      else if copy(current_input, 1, 6) = 'czysc(' then
-        processPossibleCleanse(current_input)
+    readln(input_line);
+    if valid_input_count < MAX_INPUT_LINES then begin
+      inc(valid_input_count);
+      if copy(input_line, 1, 2) = 'f(' then
+        processPossibleFuncValue(input_line)
+      else if copy(input_line, 1, 5) = 'suma(' then
+        processPossibleSum(input_line)
+      else if copy(input_line, 1, 6) = 'czysc(' then
+        processPossibleCleanse(input_line)
       else
         ignore();
-      // if you want to debug, then:
-      // writeTrees();
     end else begin
-      readln(current_input);
       ignore();
     end;
   end;
+  finalCleanup();
 end.
